69a234bc4df54cce3bd36d0d654852cb32e04b81
mszrsbotond <botond00723@gmail.com>
2025-06-10 22:44:41 +0200

    basics of ui and polished console tictactoe

--- script.js
+++ script.js
@@ -1,107 +1,116 @@
-const Gameboard = () => {
-    let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// const Gameboard = () => {
+//     let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    function addSymbol(symbol) {
-        while (true) {
-            let place = Number(prompt("Place: "))
-            place = place - 1
-            if (Number.isInteger(board[place])) {
-                board[place] = symbol
-                break
-            }
-            else {
-                alert("Place is taken!")
-            }
-        }
-    }
+//     function addSymbol(symbol) {
+//         while (true) {
+//             let place = Number(prompt("Place: "))
+//             place = place - 1
+//             if (Number.isInteger(board[place])) {
+//                 board[place] = symbol
+//                 break
+//             }
+//             else {
+//                 alert("Place is taken!")
+//             }
+//         }
+//     }
 
-    function showBoard() {
-        let boardInRows = [board.slice(0, 3), board.slice(3, 6), board.slice(6, 9)]
-        for (row of boardInRows) {
-            console.log(row)
-        }
-    }
+//     function showBoard() {
+//         let boardInRows = [board.slice(0, 3), board.slice(3, 6), board.slice(6, 9)]
+//         for (row of boardInRows) {
+//             console.log(row)
+//         }
+//     }
 
-    function getBoard() {
-        return board;
-    }
+//     function getBoard() {
+//         return board;
+//     }
 
-    return {
-        addSymbol, showBoard, getBoard
-    }
-}
+//     return {
+//         addSymbol, showBoard, getBoard
+//     }
+// }
 
-const Player = () => {
-    function makePlayer(name, symbol) {
-        return {
-            name: name,
-            symbol: symbol
-        }
-    }
+// const Player = () => {
+//     function makePlayer(name, symbol) {
+//         return {
+//             name: name,
+//             symbol: symbol
+//         }
+//     }
 
-    return {
-        makePlayer
-    }
-}
+//     return {
+//         makePlayer
+//     }
+// }
 
-const Game = (() => {
-    function checkWin(board) {
-        const winPatterns = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],
-            [0, 4, 8], [2, 4, 6]
-        ]
-        for (const pattern of winPatterns) {
-            const [a, b, c] = pattern
-            if (board[a] == board[b] && board[b] == board[c]) {
-                console.log(`${board[a]} wins!`)
-                over = true
-            }
-        }
-    }
-    function checkDraw(board) {
-        let fullBoard = false
-        for (value of board) {
-            if (Number.isInteger(value)) {
-                fullBoard = false
-                break
-            }
-            else {
-                fullBoard = true
-            }
-        }
+// const Game = (() => {
+//     function checkWin(board) {
+//         const winPatterns = [
+//             [0, 1, 2], [3, 4, 5], [6, 7, 8],
+//             [0, 3, 6], [1, 4, 7], [2, 5, 8],
+//             [0, 4, 8], [2, 4, 6]
+//         ]
+//         for (const pattern of winPatterns) {
+//             const [a, b, c] = pattern
+//             if (board[a] == board[b] && board[b] == board[c]) {
+//                 console.log(`${board[a]} wins!`)
+//                 over = true
+//             }
+//         }
+//     }
+//     function checkDraw(board) {
+//         let fullBoard = false
+//         for (value of board) {
+//             if (Number.isInteger(value)) {
+//                 fullBoard = false
+//                 break
+//             }
+//             else {
+//                 fullBoard = true
+//             }
+//         }
 
-        if (fullBoard == true) {
-            console.log("DRAW")
-            over = true
-        }
-    }
+//         if (fullBoard == true) {
+//             console.log("DRAW")
+//             over = true
+//         }
+//     }
 
 
 
-    let gameboard = Gameboard()
+//     let gameboard = Gameboard()
 
-    let p1 = Player().makePlayer("bob", "x")
-    let p2 = Player().makePlayer("doni", "o")
+//     let p1 = Player().makePlayer("bob", "x")
+//     let p2 = Player().makePlayer("doni", "o")
 
-    let currentp = p1
-    let over = false
-    gameboard.showBoard()
+//     let currentp = p1
+//     let over = false
+//     gameboard.showBoard()
 
-    while (!over) {
-        console.log("-------------------")
-        gameboard.addSymbol(currentp.symbol)
-        gameboard.showBoard()
-        checkWin(gameboard.getBoard())
-        checkDraw(gameboard.getBoard())
-        if (over) {
-            break
-        }
-        if (currentp == p1) {
-            currentp = p2
-        }
-        else {
-            currentp = p1
-        }
-    }
-})()
+//     while (!over) {
+//         console.log("-------------------")
+//         gameboard.addSymbol(currentp.symbol)
+//         gameboard.showBoard()
+//         checkWin(gameboard.getBoard())
+//         checkDraw(gameboard.getBoard())
+//         if (over) {
+//             break
+//         }
+//         if (currentp == p1) {
+//             currentp = p2
+//         }
+//         else {
+//             currentp = p1
+//         }
+//     }
+// })()
+
+
+// this will be used to get back a number and will still use the same array as database in the background just w a ui
+const boxes = document.querySelectorAll(".box")
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log(box.getAttribute("data-value"))
+    })
+})
